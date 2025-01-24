b8601d5f92403dce1a5c0e30d1d88bd9d1c328f4
MattiaLupascu <mattialeonardo.lupascu@studenti.isissgobetti.it>
2025-01-24 09:14:53 +0100

    Implement user authentication in client-server communication

--- Sincrono_Multicast/client_sincrono_multicast.py
+++ Sincrono_Multicast/client_sincrono_multicast.py
@@ -5,19 +5,32 @@ def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # Connette il client al server
     client.connect(('127.0.0.1', 5000))
-    ciclo=True
 
+    # Invia il nome utente al server per l'autenticazione
+    nome_utente = input("Inserisci il tuo nome utente: ")
+    client.send(nome_utente.encode('utf-8'))
+
+    # Riceve la risposta dal server
+    risposta = client.recv(1024).decode('utf-8')
+    if risposta == "Non autenticato":
+        print("Non autenticato")
+        client.close()
+        return
+
+    ciclo = True
     while ciclo:
         # Legge il messaggio da inviare al server
         messaggio = input("Inserisci il messaggio da inviare: ")
         # Invia il messaggio al server
         client.send(messaggio.encode('utf-8'))
-        if messaggio=="QUIT":
-            ciclo=False
-        else:    
+        if messaggio == "QUIT":
+            ciclo = False
+        else:
             # Riceve la risposta dal server
             risposta = client.recv(1024).decode('utf-8')
             print(f"Risposta del server: {risposta}")
+
+    client.close()
 
 if __name__ == "__main__":
     main()
--- Sincrono_Multicast/server_sincrono_multicast.py
+++ Sincrono_Multicast/server_sincrono_multicast.py
@@ -1,23 +1,27 @@
 import socket
 import threading
 
-def gestisci_client(socket_client,indirizzo,utenti):
-    messaggio = socket_client.recv(1024).decode('utf-8')
-    for i in utenti:
-        if messaggio==i:
-            ciclo=True
-            while ciclo:
-                try:
-                    # Riceve il messaggio dal client
-                    messaggio = socket_client.recv(1024).decode('utf-8')
-                    if messaggio=="QUIT":
-                        ciclo=False
-                    print(f"Ricevuto {indirizzo}: {messaggio}")
-                    # Invia una conferma al client
-                    socket_client.send(f"Messaggio ricevuto".encode('utf-8'))
-                except:
-                    ciclo=False
-            socket_client.close()
+def gestisci_client(socket_client, indirizzo, utenti):
+    # Riceve il primo messaggio dal client
+    utente = socket_client.recv(1024).decode('utf-8')
+    if utente not in utenti:
+        print(f"{indirizzo}: Non autenticato")
+        socket_client.send("Non autenticato".encode('utf-8'))
+        socket_client.close()
+        return  # Termina l'esecuzione della funzione e ritorna None
+
+    ciclo = True
+    while ciclo:
+        try:
+            # Riceve il messaggio dal client
+            messaggio = socket_client.recv(1024).decode('utf-8')
+            if messaggio == "QUIT":
+                ciclo = False
+            print(f"Ricevuto {utente}: {messaggio}")
+            # Invia una conferma al client
+            socket_client.send("Messaggio ricevuto".encode('utf-8'))
+        except:
+            ciclo = False
     socket_client.close()
 
 def main():
